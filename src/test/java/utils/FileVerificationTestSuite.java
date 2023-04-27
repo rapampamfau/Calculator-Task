6ff8ed118fc879e110df05150987b30261618477
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2023-04-27 20:14:40 +0200

    Test for FileVerification class and files for tests

---
+++ src/test/java/utils/FileVerificationTestSuite.java
@@ -0,0 +1,86 @@
+package utils;
+
+import com.calculator.utils.FileVerification;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+public class FileVerificationTestSuite {
+
+    private FileVerification fv = new FileVerification();
+
+    @Test
+    void testGetLastLine() {
+        //Given
+        File file = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile1.txt");
+
+        //When
+        String result = fv.getLastLine(file);
+
+        //Then
+        assertEquals("test", result);
+    }
+
+    @Test
+    void testVerifyLastLine() {
+        //Given
+        File file = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile2.txt");
+
+        //When
+        boolean result = fv.verifyLastLine(file);
+
+        //Then
+        assertTrue(result);
+    }
+
+    @Test
+    void testVerifyAllInstructions() {
+        //Given
+        File file = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile3.txt");
+
+        //When
+        boolean result = fv.verifyAllInstructions(file);
+
+        //Then
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckFileFormat() {
+        //Given
+        File file1 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile1.txt");
+        File file2 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile2.txt");
+        File file3 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile0");
+
+        //When
+        boolean res1 = fv.checkFileFormat(file1.getPath());
+        boolean res2 = fv.checkFileFormat(file2.getPath());
+        boolean res3 = fv.checkFileFormat(file3.getPath());
+
+        //Then
+        assertTrue(res1);
+        assertTrue(res2);
+        assertFalse(res3);
+    }
+
+    @Test
+    void testCheckIfFileExist() {
+        //Given
+        File file1 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile1.txt");
+        File file2 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile2.txt");
+        File file3 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\resources\\testFile0.txt");
+        File file4 = new File("C:\\Development\\Java\\Calculator-Task\\src\\test\\testFile.txt");
+
+        //When
+        boolean res1 = fv.checkIfFileExist(file1);
+        boolean res2 = fv.checkIfFileExist(file2);
+        boolean res3 = fv.checkIfFileExist(file3);
+        boolean res4 = fv.checkIfFileExist(file4);
+
+        //Then
+        assertTrue(res1);
+        assertTrue(res2);
+        assertFalse(res3);
+        assertFalse(res4);
+    }
+}
