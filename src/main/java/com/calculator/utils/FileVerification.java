19b4ed2b3afc95d02bdc27171d335cf10128671d
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2023-04-27 22:08:33 +0200

    Constraint for dividing by 0 and refactoring

--- src/main/java/com/calculator/utils/FileVerification.java
+++ src/main/java/com/calculator/utils/FileVerification.java
@@ -25,6 +25,43 @@ public class FileVerification {
         }
     }
 
+    public boolean verifyAllInstructions(File file) {
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String currentLine;
+            String previousLine = "";
+            while ((currentLine = reader.readLine()) != null) {
+                if (!previousLine.equals("")) {
+                    String[] previousLineContent = previousLine.split(" ");
+                    if (previousLineContent.length != 2) {
+                        System.out.println("Incorrect number of instructions.");
+                    }
+                    if (!checkFirstColumnSyntax(previousLineContent[0]) ||
+                            !checkSecondColumnSyntax(previousLineContent[1])) {
+                        System.out.println("Incorrect instruction syntax.");
+                    }
+                    if (!checkForDivideByZeros(currentLine)) {
+                        System.out.println("Incorrect instruction, you can't divide by 0.");
+                    }
+                }
+                previousLine = currentLine;
+            }
+            if (verifyLastLine(file)) {
+                return true;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
     public String getLastLine(File file) {
 
         String fileLastLine = "";
@@ -49,7 +86,7 @@ public class FileVerification {
         return fileLastLine;
     }
 
-    public boolean verifyLastLine(File file) {
+    private boolean verifyLastLine(File file) {
         String lastLine = getLastLine(file);
         String[] lineContent = lastLine.split(" ");
         try {
@@ -65,45 +102,33 @@ public class FileVerification {
         return false;
     }
 
-    public boolean verifyAllInstructions(File file) {
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String currentLine;
-            String previousLine = "";
-            while ((currentLine = reader.readLine()) != null) {
-                if (!previousLine.equals("")) {
-                    String[] previousLineContent = previousLine.split(" ");
-                    if (previousLineContent.length != 2) {
-                        System.out.println("Incorrect number of instructions.");
-                    }
-                    if (!checkSyntax(previousLineContent[0])) {
-                        System.out.println("Incorrect instruction syntax.");
-                    }
-                }
-                previousLine = currentLine;
-            }
-            if (verifyLastLine(file)) {
-                return true;
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (reader != null)
-                    reader.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return false;
-    }
-
-    private boolean checkSyntax(String str) {
+    private boolean checkFirstColumnSyntax(String str) {
         switch (str.toLowerCase()) {
             case "add", "subtract", "divide", "multiply" -> {
                 return true;
             }
         }
         return false;
+    }
+
+    private boolean checkSecondColumnSyntax(String str) {
+        boolean flag = false;
+        try {
+            Long.parseLong(str);
+            flag = true;
+        } catch (NumberFormatException e) {
+            System.out.println("Incorrect input, second column should be an integer");
+        }
+        return flag;
+    }
+
+    private boolean checkForDivideByZeros(String str) {
+        String[] lineContent = str.split(" ");
+        String instruction = lineContent[0];
+        long number = 0;
+        if (checkSecondColumnSyntax(lineContent[1])) {
+            number = Long.parseLong(lineContent[1]);
+        }
+        return !instruction.equalsIgnoreCase("divide") || number != 0;
     }
 }
